a6b93db6bb11d46345529a816e8c18e76fff6219
marcosabcarvalho <marcosabcarvalho@yahoo.com>
2023-10-17 17:17:01 -0300

    perfil de testes e tratamento de exceções

--- src/main/java/br/com/ideiasbits/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/ideiasbits/api/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.ideiasbits.api.services.impl;
 import br.com.ideiasbits.api.domain.User;
 import br.com.ideiasbits.api.repositories.UserRepository;
 import br.com.ideiasbits.api.services.UserService;
+import br.com.ideiasbits.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto Não encontrado"));
     }
 }
