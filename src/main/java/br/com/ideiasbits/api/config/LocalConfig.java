9cf606963cdf12a21cd29004d5625a58388de449
marcosabcarvalho <marcosabcarvalho@yahoo.com>
2023-10-17 16:49:34 -0300

    perfil de testes

---
+++ src/main/java/br/com/ideiasbits/api/config/LocalConfig.java
@@ -0,0 +1,26 @@
+package br.com.ideiasbits.api.config;
+
+import br.com.ideiasbits.api.domain.User;
+import br.com.ideiasbits.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+    @Autowired
+    private UserRepository repository;
+    @Bean
+    public void startDB() {
+        User u1 = new User(null, "Marcos Carvalho", "marcos@mail.com", "123");
+        User u2 = new User(null, "Eduardo Sergio", "edu@mail.com", "123");
+        User u3 = new User(null, "Felipe Minero", "felipe@mail.com", "123");
+
+        repository.saveAll(List.of(u1, u2, u3));
+    }
+}
